062a12ea02f56fc891f1f2ffd2af6589d9fddee6
it-takashi <takashi8816@gmail.com>
2020-05-22 11:55:14 +0900

    new create
    new.html.haml
    途中

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,19 @@
 class TweetsController < ApplicationController
   def index
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.create(tweet_params)
+    redirect_to root_path
+  end
+
+end
+
+private
+def tweet_params
+  params.require(:tweet).premit(:title, :body, :image).merge(user_id: current_user.id)
 end
