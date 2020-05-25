03ef7c3a6d9807ae99446891ec1c2fbcafe09343
it-takashi <takashi8816@gmail.com>
2020-05-25 15:40:05 +0900

    edit,destroy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+
   def index
     @tweets = Tweet.all.includes(:user)
   end
@@ -15,7 +16,24 @@ class TweetsController < ApplicationController
   def show
     @tweet = Tweet.find(params[:id])
   end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to root_path
+  end
 end
+
 
 private
 def tweet_params
