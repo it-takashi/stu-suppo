4b3574ebe7ea978d7a31c087516ad2cad1f577f2
it-takashi <takashi8816@gmail.com>
2020-05-27 13:01:14 +0900

    add show.html.haml
    formを作成
    SCC後回し

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,21 +4,21 @@ class MessagesController < ApplicationController
   def create
     @message = @teach.messages.new(message_params)
     if @message.save
-      redirect_to teach_messages_path(@teach), notice: 'メッセージが送信されました'
+      redirect_to teach_path(@teach), notice: 'メッセージが送信されました'
     else
       @messages = @teach.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
-      render :"teach/show"
+      redirect_to teach_path(@teach)
     end
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:comment, :image).merge(user_id: current_user.id)
   end
 
-  def set_group
-    @group = Teach.find(params[:teach_id])
+  def set_teach
+    @teach = Teach.find(params[:teach_id])
   end
 end
