f00f6867ef37020f26898b7ebc10681a6110e5ba
it-takashi <takashi8816@gmail.com>
2020-05-28 22:25:37 +0900

    非同期完了

--- app/assets/javascripts/teach.js
+++ app/assets/javascripts/teach.js
@@ -10,6 +10,7 @@ $(function(){
   });
   
   // カメラ映像取得
+  
   navigator.mediaDevices.getUserMedia({video: true, audio: true})
     .then( stream => {
     // 成功時にvideo要素にカメラ映像をセットし、再生
@@ -45,48 +46,49 @@ $(function(){
   });
   }
 
-  function buildHTML(messages) {
-    if ( messages.image ){
+  function buildHTML(message) {
+    if ( message.image ){
       var html = 
-        `<div class='message-list__box', data-message-id=${messages.id}>
-          <div class='message-list__box__comment-info'>
-            <div class='message-list__box__comment-info__name'>
-              ${messages.user_name}
-            </div>
-            <div class='message-list__box__comment-info__day'>
-              ${messages.created_at}
-            </div>
-          </div>
-          <div class='message-list__box__comment'>
-            <p>${messages.comment}</p>
-            <img class="lower-message__image" src=${messages.image} >
-          </div>
-        </div>`
-        return html;
+      `<div class='message-list__box', data-message-id=${message.id}>
+      <div class='message-list__box__comment-info'>
+      <div class='message-list__box__comment-info__name'>
+      ${message.user_name}
+      </div>
+      <div class='message-list__box__comment-info__day'>
+      ${message.created_at}
+      </div>
+      </div>
+      <div class='message-list__box__comment'>
+      <p>${message.comment}</p>
+      <img class="lower-message__image" src=${message.image} >
+      </div>
+      </div>`
+      return html;
     }else {
       var html = 
-      `<div class='message-list__box', data-message-id=${messages.id}>
-        <div class='message-list__box__comment-info'>
-          <div class='message-list__box__comment-info__name'>
-            ${messages.user_name}
-          </div>
-          <div class='message-list__box__comment-info__day'>
-            ${messages.created_at}
-          </div>
-        </div>
-        <div class='message-list__box__comment'>
-          <p>${messages.comment}</p>
-        </div>
+      `<div class='message-list__box', data-message-id=${message.id}>
+      <div class='message-list__box__comment-info'>
+      <div class='message-list__box__comment-info__name'>
+      ${message.user_name}
+      </div>
+      <div class='message-list__box__comment-info__day'>
+      ${message.created_at}
+      </div>
+      </div>
+      <div class='message-list__box__comment'>
+      <p>${message.comment}</p>
+      </div>
       </div>`
       return html;
     }
   }
-
+  
   $('#new_message').on('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
-
+    console.log(formData)
+    
     $.ajax({
       url: url,  //同期通信でいう『パス』
       type: "POST",  //同期通信でいう『HTTPメソッド』
@@ -95,12 +97,14 @@ $(function(){
       processData: false,
       contentType: false
     })
-
+    
     .done(function(data){
       var html = buildHTML(data);
+      console.log("dekita")
+      $('.message-list').append(html);
+      $('.form__submit').prop('disabled', false);
+      $('#new_message')[0].reset();
     })
-
+    
   })
-
-
 });
