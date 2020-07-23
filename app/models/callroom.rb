e95918ec120820edb1aed64a32e9edfc2852c806
it-takashi <takashi8816@gmail.com>
2020-07-23 15:59:07 +0900

    コメントアウト

--- app/models/callroom.rb
+++ app/models/callroom.rb
@@ -3,12 +3,6 @@ class Callroom < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
-  validates :student_id, presence:true, if: :published?
-  
-  def published?
-    student_id = user_id
-  end
-
 
   belongs_to :user
   # has_many :messages, dependent: :destroy
