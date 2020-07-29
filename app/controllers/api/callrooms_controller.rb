5c1dbccb058ada3c287e5bd873176aa3abffc26c
it-takashi <takashi8816@gmail.com>
2020-07-29 23:06:58 +0900

    headerをapplication.html.haml

--- app/controllers/api/callrooms_controller.rb
+++ app/controllers/api/callrooms_controller.rb
@@ -10,24 +10,17 @@ class Api::CallroomsController < ApplicationController
   end
 
   # 自動更新 生徒側で着信があるか
+  # @callrooms10の役割
+  ## 今のstatusの状況を見る
+  ### 3のときは、ajaxでモーダル表示
+  ### 2のときは、一度section[:callroom]で渡すことstatusが1になったとき（断られてたとき）のモーダルをajaxで表示できる。（@callroom_now）
   def new
-    
     if user_signed_in?
       if session[:callroom].present?
         @callroom_now = Callroom.find_by(id: session[:callroom]["id"])
-        # @callroom_old = session[:callroom]
       end
       @callroom10 = Callroom.find_by(student_id: current_user.id, status: 2,status: 3)
       session[:callroom] = @callroom10
     end
-
-
-    # if user_signed_in?
-    #   @callroom10 = Callroom.find_by(student_id: current_user.id)
-    #   if session[:callroom].present? && session[:callroom]["status"] == 2 && @callroom10.nil?
-
-    #   end
-    #   session[:callroom] = @callroom10
-    # end
   end
 end
